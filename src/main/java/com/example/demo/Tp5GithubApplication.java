eafda7b2a0711fffc9ea9b2cfcc1a0998031a0f6
JorgeAb83 <jorgeabm83@gmail.com>
2022-06-09 00:36:24 -0300

    Agregando

---
+++ src/main/java/com/example/demo/Tp5GithubApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import javax.swing.JOptionPane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5GithubApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(Tp5GithubApplication.class, args);
+		JOptionPane.showMessageDialog(null,"Programacion Visual");
+	}
+
+}
